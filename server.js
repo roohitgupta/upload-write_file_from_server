5159b6150d936f09a056f0fbcd2189b0fb8ceb95
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-09-12 13:55:11 +0530

    Able to read xlsx file

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = require("express")
+const express = require("express");
+const XLSX = require("xlsx");
+var workbook = XLSX.readFile("product_list.xlsx");
 // const { GetPrice } = require("./controller/controller")
 
 const app = express();
 app.use(express.json());
 
-const Router = express.Router();
+// const Router = express.Router();
 
 // app.use("/https://api.storerestapi.com/products", Router)
 // Router.get("/find/title", GetPrice)
 
+let worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
+for(let i=2; i<11; i++){
+    const name = worksheet[`A${i}`].v;
+    console.log(name);
+}
 
 
 
