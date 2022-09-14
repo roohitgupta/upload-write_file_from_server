6d48e43548dcc6e717dff1f785b387efb6769ca3
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-09-14 19:07:16 +0530

    modify whole code, Added a Form for uploding file and did some changes in writing that file.

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require("express");
+// const express = require("express");
 const XLSX = require("xlsx");
-var workbook = XLSX.readFile("product_list.xlsx");
-const axios = require('axios');
+// const axios = require('axios');
+const fs = require('fs');
 
-const app = express();
-app.use(express.json());
-
+// const app = express();
+// app.use(express.json());
 
 const getData = (name)=> {
     axios.get(`https://api.storerestapi.com/products/${name}`)
@@ -15,19 +14,53 @@ const getData = (name)=> {
     }).catch(err => console.log(err));
 };
 
+// let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+// for(let i=2; i<worksheet.length; i++){
+//     const name = worksheet[`A${i}`].v;
+//     console.log(name);
+//     getData(name);
+// };
 
-let worksheet = workbook.Sheets[workbook.SheetNames[0]];
-for(let i=2; i<11; i++){
-    const name = worksheet[`A${i}`].v;
-    // console.log(name);
-    getData(name);
-};
+// app.listen(5000, ()=> {
+//     console.log("Server is Running at Port 5000");
+// });
+
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const upload = require("express-fileupload");
+app.use(upload());
+const readXlsxFile = require("read-excel-file/node");
 
 
 
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
+app.post("/", (req, res) => {
+   if(req.files) {
+    console.log(req.files)
+    
+    const workbook = XLSX.read(req.files.file.data);
+    let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    for(let i=2; i<worksheet.length; i++){
+        const name = worksheet[`A${i}`].v;
+        console.log(name);
+        getData(name);
+    };
+    
+    console.log('worksheet', worksheet)
 
+        res.setHeader('Content-disposition', 'attachment; filename=file.xlsx');
+        res.setHeader('Content-type', 'mimetype');
+    
+        // var filestream = fs.createReadStream(new File(worksheet));
+        // filestream.pipe(res);
+    return res.download(worksheet)
+   }
+});
 
 
 
